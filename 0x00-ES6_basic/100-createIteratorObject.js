e56c28212a96033cc1b5f271c586e3dc597cc3eb
JoGMG <joattah3@gmail.com>
2023-11-29 21:15:06 +0100

    Task 13. Iterating through report objects

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,4 +1,5 @@
 export default function createIteratorObject(report) {
+  const employees = [];
   const groupedEmployees = Object.values(report.allEmployees);
-  return [].concat(...groupedEmployees);
+  return employees.concat(...groupedEmployees);
 }
