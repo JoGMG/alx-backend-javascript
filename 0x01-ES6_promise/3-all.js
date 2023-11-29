1c8531569e996428ac4d715b9c64dde3dd31001a
JoGMG <joattah3@gmail.com>
2023-11-29 23:25:27 +0100

    Task 3. Handle multiple successful promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,10 @@
+import {uploadPhoto, createUser} from './utils';
+
+export default function handleProfileSignup() {
+  const photo = uploadPhoto();
+  const user = createUser();
+  return Promise
+  	.all([photo, user])
+    .then(([resPhoto, resUser]) => console.log(`${resPhoto.body} ${resUser.firstName} ${resUser.lastName}`))
+		.catch(() => console.log('Signup system offline'));
+}
