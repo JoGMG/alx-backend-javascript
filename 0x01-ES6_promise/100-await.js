c14b6df1b34f062c6f767199bfb053714131c562
JoGMG <joattah3@gmail.com>
2023-11-30 01:10:47 +0100

    Task 10. Await / Async

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,21 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  let res = {};
+
+  try {
+    const resPhoto = await uploadPhoto();
+    const resUser = await createUser();
+    res = {
+      photo: resPhoto,
+      user: resUser
+    };
+  } catch (err) {
+    res = {
+      photo: null,
+      user: null
+    };
+  }
+
+  return res;
+}
