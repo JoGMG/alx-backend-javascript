4a7a2254506aa7695046cfcf212e1c2167af301f
JoGMG <joattah3@gmail.com>
2023-12-01 02:51:13 +0100

    Task 7. Airport

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,29 +1,7 @@
 export default class Airport {
   constructor(name, code) {
-    this.name = name;
-    this.code = code;
-  }
-
-  /**
-   * @param {String} value
-   */
-  set name(value) {
-    this._name = value;
-  }
-
-  get name() {
-    return this._name;
-  }
-
-  /**
-   * @param {String} value
-   */
-  set code(value) {
-    this._code = value;
-  }
-
-  get code() {
-    return this._code;
+    this._name = name;
+    this._code = code;
   }
 
   get [Symbol.toStringTag]() {
