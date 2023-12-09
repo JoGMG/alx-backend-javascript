c33b94423bba10d15d2fad44a2b0b33b91686c6e
JoGMG <joattah3@gmail.com>
2023-12-10 00:01:39 +0100

    Task 5

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -1,15 +1,23 @@
 export interface MajorCredits {
-  credits: number & { __brand: 'MajorCredits.credits' };
+  credits: number;
+  brand: string;
 }
 
 export interface MinorCredits {
-  credits: number & { __brand: 'MinorCredits.credits' };
+  credits: number;
+  brand: string;
 }
 
 export function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
-  return { credits: subject1.credits + subject2.credits } as MajorCredits;
+  return {
+    credits: subject1.credits + subject2.credits,
+    brand: "MajorCreditsSum",
+  };
 }
 
 export function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
-  return { credits: subject1.credits + subject2.credits } as MinorCredits;
+  return {
+    credits: subject1.credits + subject2.credits,
+    brand: "MinorCreditsSum",
+  };
 }
