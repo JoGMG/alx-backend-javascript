38221d2198dd12d7aadfc0a15c319a8b503e878f
JoGMG <joattah3@gmail.com>
2024-02-02 23:15:58 +0100

    0. Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,19 +2,23 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', function () {
-  it('integer numbers', function () {
+  it('both numbers integer', function () {
     assert.equal(calculateNumber(1, 3), 4);
   });
 
-  it('one float and one integer number', function () {
+  it('second number float and first number integer', function () {
     assert.equal(calculateNumber(1, 3.7), 5);
   });
 
-  it('two float numbers with decimal > 5', function () {
+  it('first number float and second number integer', function () {
+    assert.equal(calculateNumber(1.2, 3), 4);
+  });
+
+  it('both numbers float with decimal > 5', function () {
     assert.equal(calculateNumber(1.2, 3.7), 5);
   });
 
-  it('two float numbers with decimal < 5', function () {
+  it('both numbers float with decimal < 5', function () {
     assert.equal(calculateNumber(1.5, 3.7), 6)
   });
 });
