5fb6d18ee2ef33e80b000411ebeced93b3e9bf05
JoGMG <joattah3@gmail.com>
2024-02-03 00:10:42 +0100

    1. Combining descriptions

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,28 +1,29 @@
 /**
- * Upgrade the function you created in the previous task (`0-calcul.js`).
- * Add a new argument named `type` at first argument of the function,
- * `type` can be `SUM`, `SUBTRACT`, or `DIVIDE` (string).
- * When type is `SUM`, round the two numbers, and add `a` and `b`.
- * When type is `SUBTRACT`, round the two numbers, and subtract `b` from
- * `a`.
- * When type is `DIVIDE`, round the two numbers, and divide `a` with `b`
- * - if the rounded value of `b` is equal to 0, return the string `Error`
+ * An upgrade of the function in (`0-calcul.js`) created in the
+ * previous task.
+ * - Added a new argument named `type` as the first argument of the
+ * function, `type` can be `SUM`, `SUBTRACT`, or `DIVIDE` (string).
+ * - When type is `SUM`, `a` and `b` are rounded up and added together.
+ * - When type is `SUBTRACT`, `a` and `b` are rounded up, and `b` is
+ * subtracted from `a`.
+ * - When type is `DIVIDE`, `a` and `b` are rounded up, and `a` is
+ * divided by `b`. If the rounded value of `b` is equal to 0, a string
+ * `Error` is returned.
  */
-
 function calculateNumber(type, a, b) {
-    if (type === 'SUM') {
-        result = Math.round(a) + Math.round(b);
-        return result;
-    } else if (type === 'SUBTRACT') {
-        result = Math.round(a) - Math.round(b);
-        return result;
-    } else if (type === 'DIVIDE') {
-        if (b === 0) {
-            return 'Error';
-        }
-        result = Math.round(a) / Math.round(b);
-        return result;
+  if (type === 'SUM') {
+    result = Math.round(a) + Math.round(b);
+    return result;
+  } else if (type === 'SUBTRACT') {
+    result = Math.round(a) - Math.round(b);
+    return result;
+  } else if (type === 'DIVIDE') {
+    if (b === 0) {
+      return 'Error';
     }
+    result = Math.round(a) / Math.round(b);
+    return result;
+  }
 }
 
 module.exports = calculateNumber;
