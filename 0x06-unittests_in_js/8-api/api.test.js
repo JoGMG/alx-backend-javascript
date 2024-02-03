3848e03be4b7849d1845d25d92cffe096d198bad
JoGMG <joattah3@gmail.com>
2024-02-03 01:59:37 +0100

    8. Basic Integration testing

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -4,7 +4,7 @@ const { expect } = require('chai');
 describe('Index page', () => {
   const API_URL = 'http://localhost:7865';
 
-  it('should return correct status code', (done) => {
+  it('GET / should return correct status code and response', (done) => {
     request.get(`${API_URL}/`, (error, response, body) => {
       expect(response.statusCode).to.be.equal(200);
       expect(body).to.be.equal('Welcome to the payment system');
