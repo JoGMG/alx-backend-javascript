5b4df920c2b2b610341a12f56048f4394849ff88
JoGMG <joattah3@gmail.com>
2024-02-03 02:05:00 +0100

    9. Regex integration testing

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -2,10 +2,10 @@ const request = require('request');
 const { expect } = require('chai');
 
 describe('Index page', () => {
-  const API_URL = 'http://localhost:7865';
+  const url = 'http://localhost:7865';
 
   it('GET / should return correct status code and response', (done) => {
-    request.get(`${API_URL}/`, (error, response, body) => {
+    request.get(`${url}/`, (error, response, body) => {
       expect(response.statusCode).to.be.equal(200);
       expect(body).to.be.equal('Welcome to the payment system');
       done();
@@ -13,16 +13,16 @@ describe('Index page', () => {
   });
 
   it('GET /cart/:id should return correct status code and response for valid id', (done) => {
-    request.get(`${API_URL}/cart/12`, (_err, res, body) => {
-      expect(res.statusCode).to.be.equal(200);
+    request.get(`${url}/cart/12`, (error, response, body) => {
+      expect(response.statusCode).to.be.equal(200);
       expect(body).to.be.equal('Payment methods for cart 12');
       done();
     });
   });
 
   it('GET /cart/:id should return correct status code for invalid id', (done) => {
-    request.get(`${API_URL}/cart/hello`, (_err, res, body) => {
-      expect(res.statusCode).to.be.equal(400);
+    request.get(`${url}/cart/hello`, (error, response, body) => {
+      expect(response.statusCode).to.be.equal(404);
       done();
     });
   });
